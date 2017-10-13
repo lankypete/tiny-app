f6dc68b8b8da8f3db82cb14b0aa1138acb7b0362
lankypete <lankypete7@gmail.com>
2017-10-13 21:13:00 +0000

    Finished the user registration feature

--- express_server.js
+++ express_server.js
@@ -13,26 +13,138 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+const users = {
+  "userRandomID": {
+    id: "666",
+    email: "a@a.com",
+    password: "asdf"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
+
+//----------------------------------------
+
+
+function getUserByEmail(email) {
+  return Object.values(users).find( user => user.email === email );
+}
+
+function createUser(email, password) {
+  if(!email) { throw new Error("Please provide an email.."); }
+  if(!password) { throw new Error("Please provide a password.."); }
+  if(getUserByEmail(email)) { throw new Error("User with that email already exists.."); }
+
+  const id = generateRandomString();
+
+  users[id] = {
+    id,
+    email,
+    password
+  };
+
+  return id;
+}
+
+function getUserById(userId) {
+  return Object.values(users).find( user => user.id === userId );
+}
+
+function loginUser(email, password) {
+  const user = getUserByEmail(email);
+  if (user && user.password === password) {
+    return user;
+  } else {
+    return undefined;
+  }
+}
+
+
+
+//----------------------------------------
+
+
+
+//---------- DELETE ---------------
+
+app.get('/clearcookie', (req, res) => {
+  res.clearCookie('userId');
+  res.send('cleared cookie')
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+
+//---------Middle Ware ------------
+
+
 app.use((req, res, next) => {
-  const { username } = req.cookies;
-  res.locals.username = username;
+  const { userId } = req.cookies;
+  if ( userId ) {
+    res.locals.user = getUserById( userId );
+  }
+
+  res.locals.randomColor = randomColor();
+
   next();
+
 });
 
 app.get("/", (req, res) => {
   res.redirect('/urls')
 });
 
+//----------- Log In/Out --------------
+
+app.get("/login", (req, res) => {
+  res.render('login');
+});
+
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username)
+  const { email, password } = req.body;
+  const user = loginUser(email, password);
+
+  if( user ) {
+    res.cookie('userId', user.id);
+  }
+
   res.redirect('/urls');
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  //res.locals.username = undefined;
+  res.clearCookie('userId');
   res.redirect('/urls');
 });
+
+//------------- Register --------------
+
+app.get('/register', (req, res) => {
+
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userId = createUser(email, password);
+    res.cookie('userId', userId).redirect('/urls');
+  } catch(error) {
+    res.render('register', { error: error.message } );
+  }
+});
+
+
+//----------- URLs ------------------
+
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls : urlDatabase }
@@ -52,7 +164,6 @@ app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const newName = req.body.newName;
   urlDatabase[id] = newName;
-  console.log(urlDatabase)
   res.redirect('/urls');
 });
 
@@ -70,10 +181,6 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls");         // Respond with 'Ok' (we will replace this)
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/u/:shortURL", (req, res) => {
   const url = req.url;
   const urlKey = url.substr(url.length-6, url.length-1);
@@ -81,6 +188,10 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 
 });
+
+
+//------------------------------------------
+
 
 app.listen(PORT, () => {
  console.log(`Example app listening on port ${PORT}!`);
@@ -104,6 +215,13 @@ function generateRandomString() {
     arr.push(random);
   }
   return arr.join('');
+}
+
+
+function randomColor() {
+  return '#' + (function co(lor){   return (lor +=
+  [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
+  && (lor.length == 6) ?  lor : co(lor); })('');
 }
 
 
